6f9627cc57a4648c7ff20cda5595be7b40ed87a0
EKarpov <username@yandex.ru>
2026-05-19 12:15:28 +0400

    6 commit

--- tests/test_create_recipe.py
+++ tests/test_create_recipe.py
@@ -2,7 +2,7 @@ import allure
 from pathlib import Path
 #from pages.upload_page import UploadPage
 from generators import Generatorss
-import time
+#import time
 
 
 
@@ -29,7 +29,7 @@ class TestCreateRecipePage:
         recipe_page.click_create_recipe_button()
         recipe_name_from_new_card = recipe_page.get_recipe_name_after_create_recipe()
 
-        assert recipe_name == recipe_name_from_new_card
+        assert recipe_name == 2 #recipe_name_from_new_card
 
     
 
@@ -39,6 +39,6 @@ class TestCreateRecipePage:
 
     ## Загрузит фото
 
-    @allure.step("Нажать кнопку Создать рецепт")
-    def click_create_recipe_button(self):
-        self.click_to_element(CreateRecipePageLocators.CREATE_RECIPE_BUTTON)
+   # @allure.step("Нажать кнопку Создать рецепт")
+   # def click_create_recipe_button(self):
+   #     self.click_to_element(CreateRecipePageLocators.CREATE_RECIPE_BUTTON)
